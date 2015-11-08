a56558801d94136a5c2e6e427cd779366489d14e
Andy <andy@Andys-MacBook-Air.local>
2015-11-07 22:53:41 -0500

    Added Bubble sort

--- Sorting.java
+++ Sorting.java
@@ -3,7 +3,24 @@ public class Sorting
    public static void main(String[] args)
    {
       int[] arr = {3,5,4,2,1};
+      bubble(arr);
       print(arr);
+   }
+   public static void bubble(int[] arr)
+   {
+      boolean isSwap = true;
+      while(isSwap == true)
+      {
+         isSwap = false;
+         for(int i = 1; i < arr.length; i++)
+         {
+            if(arr[i] < arr[i-1])
+            {
+               swap(arr, i, i-1);
+               isSwap=true;
+            }
+         }
+      }
    }
    public static void swap(int[] arr, int fromIdx, int toIdx)
    {
