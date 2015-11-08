41eec3ade425ac2dc25287fd5f833613e618ce33
Andy <andy@Andys-MacBook-Air.local>
2015-11-07 22:26:36 -0500

    Initial Commit

---
+++ Sorting.java
@@ -0,0 +1,15 @@
+public class Sorting
+{
+   public static void main(String[] args)
+   {
+      int[] arr = {3,5,4,2,1};
+      print(arr);
+   }
+   public static void print(int[] arr)
+   {
+      for(Integer i : arr)
+      {
+         System.out.println(i);
+      }
+   }
+}
